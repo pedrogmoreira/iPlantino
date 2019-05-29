2c56f0115881974e7b0db8461d8b2f6e192d47cb
Pedro Henrique <pedro.moreira@egl.eng.br>
2019-05-29 10:06:53 -0300

    Infra.Data

---
+++ src/iPlantino.Infra.Data/Helpers/MappingHelper.cs
@@ -0,0 +1,40 @@
+﻿using iPlantino.Domain.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iPlantino.Infra.Data.Helpers
+{
+    public static class MappingHelper
+    {
+        public static void ConfigureKey<T>(this EntityTypeBuilder<T> entity, string columnsName) where T : Entity
+        {
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Id)
+                .HasDefaultValueSql("newid()");
+
+            entity.Property(e => e.Id)
+               .HasColumnName(columnsName);
+        }
+
+        public static void AddInclusionDate<T>(this EntityTypeBuilder<T> entity) where T : Entity
+        {
+            entity.Property(e => e.CreatedAt)
+                .HasColumnName("inclusion_date")
+                .HasDefaultValueSql("getutcdate()");
+
+            //entity.Property(e => e.UpdatedAt)
+            //    .HasColumnName("data_atualizacao")
+            //    .HasDefaultValueSql("Now()");
+        }
+
+        public static void IgnoreInclusion<T>(this EntityTypeBuilder<T> entity) where T : Entity
+        {
+            entity.Ignore(e => e.CreatedAt);
+            //entity.Ignore(e => e.UpdatedAt);
+        }
+    }
+}
---
+++ src/iPlantino.Infra.Data/Mappings/Authentication/UserGroupEntityMapping.cs
@@ -0,0 +1,34 @@
+﻿using iPlantino.Domain.Models.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace iPlantino.Infra.Data.Mappings
+{
+    public class UserGroupEntityMapping : IEntityTypeConfiguration<UserGroup>
+    {
+        public void Configure(EntityTypeBuilder<UserGroup> entity)
+        {
+            entity.ToTable("tbl_user_group", "authentication");
+
+            entity.HasKey(x => new { x.IdGroup, x.IdUser });
+
+            entity.Property(e => e.IdGroup)
+                .HasColumnName("id_group");
+
+            entity.Property(e => e.IdUser)
+                .HasColumnName("id_user");
+
+            entity.HasOne(d => d.Group)
+                .WithMany(p => p.UsersGroup)
+                .HasForeignKey(d => d.IdGroup)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_user_group_group");
+
+            entity.HasOne(d => d.User)
+                .WithMany(p => p.UsersGroup)
+                .HasForeignKey(d => d.IdUser)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_user_group_user");
+        }
+    }
+}
